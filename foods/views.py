7b42ec117a21f4a55f448327b4f99ab5822d1a21
masood eghbali <masoodegh@gmail.com>
2022-03-01 17:35:16 +0330

    complet

--- foods/views.py
+++ foods/views.py
@@ -3,18 +3,12 @@ from django.shortcuts import render
 from foods.models import food
 
 # Create your views here.
-def list_food(request):
+
+def list_foot(request):
     query=food.objects.all()
     context={
-        'object':query
+        "object":query
     }
-    return render(request,'index.html',context)
-def detail_food(request,ID):
-    query=food.objects.get(id=ID)
-    context={
-        'object':query
-    }
-    return render(request,'foods/detail_food.html',context)
+    return render(request,"index.html",context)
+    
 
-class about(TemplateView):
-    template_name = "about.html"
